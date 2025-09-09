631cd07eb37c6aa57c789f905036fe45e2f34fc0
Siddharth Patwardhan <siddpp16@gmail.com>
2025-09-09 22:56:11 +0530

    feat: passed for // signs

--- calculator.js
+++ calculator.js
@@ -1,10 +1,18 @@
-function add(number){
-    if(number === ''){
-        return 0;
-    }
-const numArray = number.split(/,|\n/); 
+function add(number) {
+  if (number === "") {
+    return 0;
+  }
+  let delimiter = /,|\n/;
+  let numbersString = number;
 
-return numArray.reduce((sum, num) => sum + parseInt(num), 0);
+  if (number.startsWith("//")) {
+    const parts = number.split("\n");
+    delimiter = parts[0].substring(2);
+    numbersString = parts[1]; // Use only the numbers part, not the whole string
+  }
+
+  const numArray = numbersString.split(delimiter);
+  return numArray.reduce((sum, current) => sum + parseInt(current), 0);
 }
 
-module.exports = {add  };
+module.exports = { add };
