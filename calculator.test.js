2301d7669dde103fd0ba0fa4b323b9390d50960f
Siddharth Patwardhan <siddpp16@gmail.com>
2025-09-09 22:45:57 +0530

    feat: Passed for single number

--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,5 @@
 const { add } = require("./calculator");
 
-ttest('should return the number when only one number is in the string', () => {
+test('should return the number when only one number is in the string', () => {
     expect(add("1")).toBe(1);
   });
