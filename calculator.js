4f43e4a643f76f66c398bece1492979f5bf2f1d5
Siddharth Patwardhan <siddpp16@gmail.com>
2025-09-09 22:51:18 +0530

    feat: newLine addition of numbers passed

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ function add(number){
     if(number === ''){
         return 0;
     }
-const numArray = number.split(',');
+const numArray = number.split(/,|\n/); 
 
 return numArray.reduce((sum, num) => sum + parseInt(num), 0);
 }
